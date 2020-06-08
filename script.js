60c3266cd815c5b1eefcffdfb678891a9693a1f4
mrrezaahmadi <mrrezaahmadi>
2020-06-08 18:19:22 +0430

    fix the bug when the keys are clicked, the color of key changed but don't back to privious state

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ keys.forEach((key) => {
 });
 
 const playNote = (key) => {
-    const noteAudio = document.getElementById(key.dataset.note)
-    noteAudio.play();
-    key.classList.add('active')
-}
+	const noteAudio = document.getElementById(key.dataset.note);
+	noteAudio.play();
+	key.classList.add("active");
+	noteAudio.addEventListener("ended", () => {
+		key.classList.remove("active");
+	});
+};
